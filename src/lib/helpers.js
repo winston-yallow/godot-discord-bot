6feea50df385a301dc4772519f32996eeab2c65e
Winston <44872771+winston-yallow@users.noreply.github.com>
2023-06-17 21:01:45 +0200

    Implement helper for Godot styled embeds

---
+++ src/lib/helpers.js
@@ -0,0 +1,18 @@
+const { EmbedBuilder } = require('discord.js');
+
+/**
+ * EmbedBuilder subclass with default godot style
+ */
+class GodotEmbedBuilder extends EmbedBuilder {
+	/**
+	 * Creates a new embed builder that uses the Godot blue color
+	 * and includes a timestamp.
+	 */
+	constructor() {
+		super();
+		this.setColor(0x458dc0);
+		this.setTimestamp();
+	}
+}
+
+module.exports = { GodotEmbedBuilder };
