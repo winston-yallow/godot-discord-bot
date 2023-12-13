791ba9327a058c2df7c7f6e5be06ac0c2b95d7c9
Winston <44872771+winston-yallow@users.noreply.github.com>
2023-12-13 23:33:12 +0100

    Move tuxfamily to the bottom of download options

--- src/units/downloads.js
+++ src/units/downloads.js
@@ -6,10 +6,10 @@ const embed = new GodotEmbedBuilder()
 	.setDescription('You can download Godot from the following websites and platforms:')
 	.addFields([
 		{ name: 'Official Website', value: 'https://godotengine.org/download/' },
-		{ name: 'Tuxfamily', value: 'https://downloads.tuxfamily.org/godotengine/' },
 		{ name: 'GitHub', value: 'https://github.com/godotengine/godot/releases' },
 		{ name: 'Itch', value: 'https://godotengine.itch.io/godot' },
 		{ name: 'Steam', value: 'https://store.steampowered.com/app/404790/Godot_Engine/' },
+		{ name: 'Tuxfamily', value: 'https://downloads.tuxfamily.org/godotengine/' },
 	])
 ;
 
