321705c6d8bf149e4106067ae678b633ce9e61ab
Winston <44872771+winston-yallow@users.noreply.github.com>
2023-12-14 02:26:44 +0100

    Remove obsolete comment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ const { ModularClient } = require('./lib/client');
 
 const config = require('./config');
 
-// TODO: Config file should also specify the units to load
 const client = new ModularClient(config);
 client.login();
