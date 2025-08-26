d8b4bfcc657b254acc6be5f90020c68b74766620
Loki <127683587+LokiLeiche@users.noreply.github.com>
2025-08-27 00:00:00 +0200

    GDScript command

---
+++ src/units/gdscript.js
@@ -0,0 +1,25 @@
+const { Unit } = require('../lib/units.js');
+const { GodotEmbedBuilder } = require('../lib/helpers.js');
+
+const embedRepost = new GodotEmbedBuilder()
+    .setTitle('Learning GDScript')
+    .setDescription(
+        'To get started with learning GDScript, you should '
+        + 'take a look at the official documentation: '
+        + 'https://docs.godotengine.org/en/stable/tutorials/scripting/gdscript/gdscript_basics.html.\n\n'
+        + 'Another good resource for learning would be https://gdquest.github.io/learn-gdscript/'
+    )
+;
+
+const unit = new Unit();
+
+unit.createCommand()
+    .setName('gdscript')
+    .setRateLimit(10)
+    .setDescription('Explains to not ask in multiple channels')
+    .setCallback(async interaction => {
+        await interaction.reply({ embeds: [embedRepost] })
+    })
+;
+
+module.exports = unit;
