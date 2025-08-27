5f09934d7a88cf1bd1eaddeba1e5f191a943bad5
Loki <127683587+LokiLeiche@users.noreply.github.com>
2025-08-27 19:59:03 +0200

    requested changes

--- src/units/context-report.js
+++ src/units/context-report.js
@@ -14,15 +14,15 @@ unit.createContextMenuCommand()
 		const modChannel = unit.config.guildConfigs[interaction.guildId].modChannel;
 		const modRole = unit.config.guildConfigs[interaction.guildId].modRole;
 		const channel = interaction.client.channels.cache.get(modChannel);
-		const reportedMessage = await channel.messages.fetch(interaction.targetId);
+		const reportedMessage = interaction.targetMessage;
 		const embed = new GodotEmbedBuilder()
-			.setAuthor({ name: interaction.user.username, iconURL: interaction.user.avatarURL() ?? "https://discord.com/assets/320d5a40d309f942.png" })
-			.setDescription(reportedMessage.content || "No content");
+			.setAuthor({ name: interaction.user.username, iconURL: interaction.user.avatarURL() ?? interaction.user.defaultAvatarURL })
+			.setDescription(reportedMessage.content || 'No content');
 		const msg = `<@&${modRole}> New report created by <@${interaction.user.id}>\n`
 		+ `:link:Link: https://discord.com/channels/${interaction.guildId}/${interaction.channelId}/${interaction.targetId}`;
-		const files = []
+		const files = [];
 		reportedMessage.attachments.forEach(attachment => {
-			files.push(attachment.url)
+			files.push(attachment.url);
 		});
 		channel.send({ content: msg, embeds: [embed], files: files });
 		await interaction.reply({ flags: MessageFlags.Ephemeral, content: 'Message has been reported. A staff member will check soon.' });
